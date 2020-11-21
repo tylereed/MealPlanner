d34d17a906f71944d398386034adfca5fe43052c
Tyler <tylereed@gmail.com>
2020-11-21 10:05:09 -0700

    Adding in Recipe.vue

--- mealplanner-web/src/App.vue
+++ mealplanner-web/src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Recipes />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Recipes from "@/pages/Recipes.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Recipes
   }
-}
+};
 </script>
 
 <style>
