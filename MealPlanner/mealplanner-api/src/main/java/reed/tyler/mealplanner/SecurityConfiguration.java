e43998e614a151324ee4f8d1ea7fd2a3ab208f48
Tyler <tylereed@gmail.com>
2020-11-21 21:48:20 -0700

    Fixed spelling of security, removed favicon

--- MealPlanner/mealplanner-api/src/main/java/reed/tyler/mealplanner/SecurityConfiguration.java
+++ MealPlanner/mealplanner-api/src/main/java/reed/tyler/mealplanner/SecurityConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SecutiryConfiguration {
+public class SecurityConfiguration {
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
