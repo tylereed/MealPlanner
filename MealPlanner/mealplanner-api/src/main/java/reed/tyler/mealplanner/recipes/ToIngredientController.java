d38ca22842fc5f264d0c5067569a5e17f1540875
Tyler <tylereed@gmail.com>
2020-11-27 16:36:58 -0700

    Implemented set ingredients endpoint

--- MealPlanner/mealplanner-api/src/main/java/reed/tyler/mealplanner/recipes/ToIngredientController.java
+++ MealPlanner/mealplanner-api/src/main/java/reed/tyler/mealplanner/recipes/ToIngredientController.java
@@ -1,8 +1,11 @@
 package reed.tyler.mealplanner.recipes;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +13,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import reed.tyler.mealplanner.ingredients.Ingredient;
 import reed.tyler.mealplanner.ingredients.IngredientRepository;
+import reed.tyler.mealplanner.utils.ErrorMessages;
 
 @RestController
 @RequestMapping("/api/recipes/{recipeId}/ingredients")
@@ -28,8 +33,31 @@ public class ToIngredientController {
 	}
 
 	@PostMapping
-	public ResponseEntity<?> set(@RequestBody List<String> ingredientNames) {
-		return ResponseEntity.status(500).build();
+	public ResponseEntity<?> set(@PathVariable long recipeId, @RequestBody Set<Long> ingredientIds) {
+		var recipe = recipesRepo.findById(recipeId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+		var ingredients = ingredientsRepo.findAllById(ingredientIds).stream().collect(Collectors.toSet());
+
+		if (ingredientIds.size() > ingredients.size()) {
+			var dbIngredients = ingredients.stream().map(i -> i.getId()).collect(Collectors.toSet());
+			ingredientIds.removeAll(dbIngredients);
+
+			boolean plural = ingredientIds.size() > 1;
+			String missingIds = ingredientIds.stream().map(String::valueOf).collect(Collectors.joining("', '"));
+			
+			String message;
+			if (plural) {
+				message = String.format("Ingredients with ids '%s' do not exist", missingIds);
+			} else {
+				message = String.format("An Ingredient with id '%s' does not exist", missingIds);
+			}
+			
+			var errors = new ErrorMessages(message);
+			return ResponseEntity.badRequest().body(errors);
+		}
+
+		recipe.setIngredients(ingredients);
+		recipe = recipesRepo.saveAndFlush(recipe);
+		return ResponseEntity.noContent().build();
 	}
 
 	@GetMapping
